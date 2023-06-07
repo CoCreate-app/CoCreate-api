447e3883fa5f08fb302e62dd88dc5f299df9ae00
frankpagan <frank@cocreate.app>
2023-06-06 19:15:40 -0600

    @cocreate/cli/check-coc.js to install gloablly if does not exist

--- src/server.js
+++ src/server.js
@@ -4,106 +4,106 @@ let socket = new socketClient("ws");
 
 crud.setSocket(socket);
 
-var api = ( ()=> {
-  return {
-  	send_response: (wsManager, socket, obj, send_response) => {
-	    wsManager.send(socket, send_response, obj)
-	},
-	  
-	handleError: (wsManager, socket, action, error, component) => {
-		const response = {
-			'object': 'error',
-			'data':error || error.response || error.response.data || error.response.body || error.message || error,
-		};
-		wsManager.send(socket, component, { action, response })
-	},
+var api = (() => {
+    return {
+        send_response: (wsManager, socket, obj, send_response) => {
+            wsManager.send(socket, send_response, obj)
+        },
 
-	getOrg: async (config, component) => {
+        handleError: (wsManager, socket, action, error, component) => {
+            const response = {
+                'object': 'error',
+                'data': error || error.response || error.response.data || error.response.body || error.message || error,
+            };
+            wsManager.send(socket, component, { action, response })
+        },
 
-		socket.create({
-			namespace: config["organization_id"],
-			room: null,
-			host: config["host"]
-		})
+        getOrg: async (config, component) => {
 
-		let org = await crud.readDocument({
-	        collection: "organizations",
-	        key: config["key"],
-		    organization_id: config["organization_id"],
-			document: {
-				_id: config["organization_id"]
-			}
+            socket.create({
+                namespace: config["organization_id"],
+                room: null,
+                host: config["host"]
+            })
 
-	    });
+            let org = await crud.readDocument({
+                collection: "organizations",
+                key: config["key"],
+                organization_id: config["organization_id"],
+                document: {
+                    _id: config["organization_id"]
+                }
 
-		if (!org || !org.document && !org.document[0]) {
-			console.log(component," Error GET ORG  in : ",e);
-			return false;
-		}
+            });
 
-		return org.document[0];
-	},
+            if (!org || !org.document && !org.document[0]) {
+                console.log(component, " Error GET ORG  in : ", e);
+                return false;
+            }
 
-	getOrgInRoutesbyHostname : async (config, hostname) => {
- 		var socket_config = {
-  		    "config": {
-  		        "key": config["config"]["key"],
-  		        "organization_id": config["config"]["organization_id"],
-  		    },
-  		    "prefix": "ws",
-  		    "host": "server.cocreate.app:8088"
-  		};
-  		
-		socket.create({
-			namespace: socket_config.config.organization_id,
-			room: null,
-			host: socket_config.host
-		})
-	
-		let data2 = await crud.readDocument({
-			collection: "organizations",
-			filter: {
-				query: [{
-					name: 'domains',
-					operator: "$in",
-					value: [hostname]
-				}],
-			},
-			key: config["config"]["key"],
-			organization_id: config["config"]["organization_id"]
-		});
-	
-		var org = data2.document[0]
+            return org.document[0];
+        },
 
-		var socket_config = {
-			"config": {
-				"key": org["key"],
-				"organization_id": org["_id"].toString(),
-			},
-			"prefix": "ws",
-			"host": "server.cocreate.app:8088"
-		}
+        getOrgInRoutesbyHostname: async (config, hostname) => {
+            var socket_config = {
+                "config": {
+                    "key": config["config"]["key"],
+                    "organization_id": config["config"]["organization_id"],
+                },
+                "prefix": "ws",
+                "host": "server.cocreate.app:8088"
+            };
 
-		//other connection
-		socket.create({
-		  	namespace: socket_config.config.organization_id,
-			room: null,
-			host: socket_config.host
-		})
+            socket.create({
+                namespace: socket_config.config.organization_id,
+                room: null,
+                host: socket_config.host
+            })
 
-		let myOrg = await crud.readDocument({
-			collection: "organizations",
-			key: org["key"],
-			organization_id: org["_id"],
-			document: {
-				_id: org["_id"]
-			}
-		});
-	    let result = {'row':myOrg,'socket_config':socket_config};
-	    return result;
-	 }  
-	
-  }
+            let data2 = await crud.readDocument({
+                collection: "organizations",
+                filter: {
+                    query: [{
+                        name: 'hosts',
+                        operator: "$in",
+                        value: [hostname]
+                    }],
+                },
+                key: config["config"]["key"],
+                organization_id: config["config"]["organization_id"]
+            });
+
+            var org = data2.document[0]
+
+            var socket_config = {
+                "config": {
+                    "key": org["key"],
+                    "organization_id": org["_id"].toString(),
+                },
+                "prefix": "ws",
+                "host": "server.cocreate.app:8088"
+            }
+
+            //other connection
+            socket.create({
+                namespace: socket_config.config.organization_id,
+                room: null,
+                host: socket_config.host
+            })
+
+            let myOrg = await crud.readDocument({
+                collection: "organizations",
+                key: org["key"],
+                organization_id: org["_id"],
+                document: {
+                    _id: org["_id"]
+                }
+            });
+            let result = { 'row': myOrg, 'socket_config': socket_config };
+            return result;
+        }
+
+    }
 })();
 
 module.exports = api;
