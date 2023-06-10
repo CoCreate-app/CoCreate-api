6d99cce6c5275b0d8d1a05e73ed70cb9b455d8bc
frankpagan <frank@cocreate.app>
2023-06-10 16:16:23 -0600

    fix: renamed hosts to host. the value can be a string or an array of strings

--- src/server.js
+++ src/server.js
@@ -64,7 +64,7 @@ var api = (() => {
                 collection: "organizations",
                 filter: {
                     query: [{
-                        name: 'hosts',
+                        name: 'host',
                         operator: "$in",
                         value: [hostname]
                     }],
