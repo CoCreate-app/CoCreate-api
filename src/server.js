93d028877f4c2bdccc6c02ee7435620c8c8b2bab
frankpagan <frank@cocreate.app>
2024-01-08 01:23:18 +0000

    fix: applied host to define environment/branch

--- src/server.js
+++ src/server.js
@@ -7,53 +7,13 @@ class CoCreateApi {
 
     async getOrganization(config, component) {
 
-        let org = await CRUD.send({
-            method: 'object.read',
-            array: "organizations",
-            key: config["key"],
-            organization_id: config["organization_id"],
-            object: {
-                _id: config["organization_id"]
-            }
+        let organization = await this.crud.getOrganization(config.organization_id);
 
-        });
-
-        if (!org || !org.object && !org.object[0]) {
-            console.log(component, " Error GET ORG  in : ", e);
+        if (!organization.error)
             return false;
-        }
 
-        return org.object[0];
+        return organization
     }
-
-    async getApiKey(organization_id, name) {
-        this.organizations[organization_id] = this.getOrganization(organization_id, name)
-        this.organizations[organization_id] = await this.organizations[organization_id]
-        return this.organizations[organization_id]
-    }
-
-    async getOrganizationNew(organization_id, name) {
-        let organization = await this.crud.send({
-            method: 'object.read',
-            database: organization_id,
-            array: 'organizations',
-            object: [{ _id: organization_id }],
-            organization_id
-        })
-
-        if (organization
-            && organization.object
-            && organization.object[0]) {
-            if (organization.object[0].apis && organization.object[0].apis[name]) {
-                return organization.object[0].apis && organization.object[0].apis[name]
-            } else
-                return { [this.name]: false, error: 'An apikey could not be found' }
-        } else {
-            return { serverOrganization: false, error: 'An organization could not be found' }
-        }
-
-    }
-
 }
 
 
